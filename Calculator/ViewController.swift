3ec93bc6c9d4cc89f633019ad3bdbf65a08cc292
rudiwijaya <rudi.wijaya@mitrais.com>
2019-02-21 10:42:47 +0700

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Rudi Wijaya on 21/02/19.
+//  Copyright © 2019 Rudi Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
